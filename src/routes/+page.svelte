a9145552d629319f90a8762aea55f3a644b5cc5f
Simon Ryrie <simon@thisissoon.com>
2024-11-27 21:28:48 +0000

    feat(LandingPage): Update text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Iain Peters' website for The Corridor</h1>
