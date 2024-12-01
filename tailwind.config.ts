6772f5d540d63272ec6af981fe48b6396f60b1ba
Simon Ryrie <simon@thisissoon.com>
2024-12-01 17:23:33 +0000

    feat(Styles): Update CSS and Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,29 @@ import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		fontFamily: {
+			sans: ['Thasadith'],
+			serif: ['Bentham'],
+			body: ['Thasadith']
+		},
+		extend: {
+			fontFamily: {
+				Courgette: ['courgette'],
+				ContrailOne: ['contrail-one'],
+				Bentham: ['bentham'],
+				Signika: ['signika'],
+				Thasadith: ['thasadith'],
+				Cabin: ['cabin']
+			},
+			colors: {
+				'off-white': '#ebe9e6',
+				'dark-brown': '#241c11'
+			}
+		}
+	},
 
-  plugins: [typography, forms, containerQueries, aspectRatio]
+	plugins: [typography, forms, containerQueries, aspectRatio]
 } as Config;
