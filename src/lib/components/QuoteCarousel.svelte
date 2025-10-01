22b96a80b1a82b8459f42144b0fb1b87ee740df7
Simon Ryrie <simon@thisissoon.com>
2025-10-01 21:25:25 +0100

    feat(Quotes): Update reviews and quote carousel

--- src/lib/components/QuoteCarousel.svelte
+++ src/lib/components/QuoteCarousel.svelte
@@ -24,23 +24,22 @@
 			}
 		},
 		autoplay: {
-			delay: 3000,
+			delay: 5000,
 			disableOnInteraction: false
 		}
 	};
 
 	const quotes = [
 		'The Corrior is a beautifully written account of a desperate struggle for survival. I urge you to read it.',
-		'...a beautifully written account of a desperate struggle for survival. I urge you to read it.',
 		'Once started I couldn’t put it down. A remarkable, haunting book, it is so much more than a climber’s memoir.',
-		"[Peters'] pitch perfect writing leads you into his world...",
 		'It’s a great story, the story of a life well-lived... takes the breath away. A compelling narrative of resilience and heroism, on and off the rock...',
 		'...a joyful book and one of the most moving autobiographies that I have read, full of both deeply felt passion and compassion for others.',
-		'Intelligent and compassionate.',
 		'This is a book that had to be written... I cannot stress how important this book is.',
+		'Intelligent and compassionate.',
 		'Peters’ writing is excellent and imaginative... in this searingly frank and moving book, and it deserves to be read by the widest possible audience.',
 		'The book is different. frank, moving, and ultimately hopeful testimony. I strongly recommend it.',
-		'An outstanding book.'
+		'An outstanding book.',
+		"[Peters'] pitch perfect writing leads you into his world..."
 	];
 
 	onMount(() => {
@@ -57,7 +56,7 @@
 		{#each quotes as quote}
 			<swiper-slide>
 				<div
-					class="p-6 bg-dark-brown/90 text-white text-center h-24 mx-4 flex justify-center items-center rounded-full"
+					class="p-6 bg-dark-brown/90 text-white text-center h-32 md:h-24 mx-4 flex justify-center items-center rounded-full"
 				>
 					<p>{`"${quote}"`}</p>
 				</div>
--- src/lib/content/content.ts
+++ src/lib/content/content.ts
@@ -19,14 +19,33 @@ export type Quote = {
 };
 export const quotes: Quote[] = [
 	{
-		quote: `…in five decades of devouring climbing literature, I’ve never read anything that comes close to the emotional impact of Iain’s life story. …this extraordinary and ultimately uplifting book. It challenges the reader and is not easy reading at times, but it offers a remarkable story of survival, a beautiful sense of time and place, the value of strong non-judgemental relationships, and the joy of the outdoors`,
+		quote: `…in five decades of devouring climbing literature, I’ve never read anything that comes close to the emotional impact of Iain’s life story. …this extraordinary and ultimately uplifting book… offers a remarkable story of survival, a beautiful sense of time and place, the value of strong non-judgemental relationships, and the joy of the outdoors and time spent on rock and ice.`,
 		author: 'Hugh Thornbery',
 		position: 'Professional Mountaineering Magazine'
 	},
 	{
-		quote: `… Iain’s book is different. Though it is at times an undeniably uncomfortable read, it is also a frank, moving, and ultimately hopeful testimony. I strongly recommend it.`,
-		author: 'Tony Page',
-		position: 'Consultant Psychiatrist (Ret), British Mountain Medicine Society'
+		quote: `… This is a book that had to be written… I cannot stress how important this book is.`,
+		author: 'Toni Carver',
+		position: 'Editor, The St Ives Times and Echo'
+	},
+	{
+		quote: `Iain Peters’ book is about the survival, recovery, and creation of a life after being
+		sexually abused as a child. It is beautifully written, full of detail that paints a vivid
+		picture of place, time, events, and relationships without a consuming focus on the
+		brutality or any attempt to elicit emotion in the reader. It is both intelligent and
+		compassionate, portraying the long-term impact of the abuse through his life as a
+		climber and offering critical analysis and deep insight into his own behaviour patterns
+		and responses. Of the many aspects that make this an outstanding book are how he
+		understands his rejection of power, authority and the values that sustain those
+		systems, his identification of the crucial factors necessary to recover and thrive, and
+		his explanation of why it is so difficult to talk about sexual assault and why people
+		stay in or return to an abusive relationship. The focus on survival strategies and
+		reflective analysis is what makes it possible to read this book without falling into a pit
+		of despair and instead to recognise a life fulfilled and lived with joy despite its early
+		trauma.`,
+		author: 'Dr Joanna Liddle',
+		position: `Co-founder of the Centre for the Study of Women and Gender, University of Warwick
+		Founder member of Hereford Women’s Aid refuge`
 	},
 	{
 		quote: `… The Corridor is full of brave deeds and derring-do, but it is the courage to bring the full story out into the open, and set it down in print, which takes the breath away. The result is a compelling narrative of resilience and heroism… it is a beacon of hope, and an inspiration to move forward.`,
@@ -39,20 +58,29 @@ export const quotes: Quote[] = [
 		position: 'Author and Contributor, UKC Online'
 	},
 	{
-		quote: `… Once started I couldn’t put it down. A remarkable, haunting book… in the end, a joyful book and one of the most moving autobiographies that I have read, full of both deeply felt passion and compassion for others`,
+		quote: `’The Corridor’, a memoir about the trauma of childhood sexual abuse, and the
+		profound influence that had on the author, a leading UK climber and teacher, isn’t
+		easy to put down. His pitch perfect writing leads you into his world; I was left feeling I
+		had been on that fearful corridor and experienced almost first-hand the thrill of the
+		cliff edge, the deliberate turning away from the prescribed English boarding school
+		route to Cambridge, and all the adventures that followed. There is a universal quality
+		to the writing, which makes it widely accessible and gives voice - and hope - to the
+		men and women who have experiences that echo Iain Peters’ own but struggle to
+		articulate those dark places.`,
+		author: 'Dr Elizabeth Peretz',
+		position:
+			'Associate Fellow, Department of Social Policy and Intervention, University of Oxford.'
+	},
+	{
+		quote: `This book is a rollercoaster of emotions from start to finish. Iain writes with an expressive clarity that just draws you in like no other book I've read. I couldn't put it down. What a story.`,
+		author: 'Mark Garland',
+		position: 'Founder, Garland Mountaineering'
+	},
+	{
+		quote: `… Once started I couldn’t put it down. A remarkable, haunting book… in the end, a joyful book and one of the most moving autobiographies that I have read, full of both deeply felt passion and compassion for others.`,
 		author: 'Dr. Terry Gifford',
 		position:
 			'Profesor Honorifico, Filologia Inglesa, Universidad de Alicante, Spain; Reader in Literature and Environment, University of Leeds (retired)'
-	},
-	{
-		quote: `… This is a book that had to be written… I cannot stress how important this book is.`,
-		author: 'Toni Carver',
-		position: 'Editor, The St Ives Times and Echo'
-	},
-	{
-		quote: `… The writing is excellent and imaginative… in this searingly frank and moving book, and it deserves to be read by the widest possible audience.`,
-		author: 'Nick Shearman',
-		position: 'TV Producer, retired'
 	},
 	{
 		quote: `… It’s a really superb work; a magnum opus indeed.`,
@@ -60,9 +88,14 @@ export const quotes: Quote[] = [
 		position: 'Mountaineer, Alpine Club member'
 	},
 	{
+		quote: `… Iain’s book is different. Though it is at times an undeniably uncomfortable read, it is also a frank, moving, and ultimately hopeful testimony. I strongly recommend it.`,
+		author: 'Tony Page',
+		position: 'Consultant Psychiatrist (Ret), British Mountain Medicine Society'
+	},
+	{
 		quote: `… A most compelling book and will offer something to everyone, with reflections belonging only to themselves and this is the power and skill of Iain's narrative. A critical book for everyone who just wants a good read, but an important book which could be used as a case study in training for police, social workers, health personnel and youth workers as well as an academic text for students of the social sciences.`,
 		author: 'Linda Moore',
-		position: 'Retired social worker, Facebook Jan 2025'
+		position: 'Retired Health Visitor'
 	},
 	{
 		quote: `… A well-written and imaginative book, searingly frank… it deserves to be read by the widest possible audience.`,
