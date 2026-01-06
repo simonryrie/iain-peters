4f1a47f157138e1ce0f942a63ffbc85a6cc37123
Simon Ryrie <simon@thisissoon.com>
2026-01-06 10:57:48 +0000

    feat(Press and Reviews page): Update name to Press and Reviews

--- src/routes/press-and-reviews/+page.svelte
+++ src/routes/press-and-reviews/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main class="px-6 md:px-12 xl:px-24 py-12 flex flex-col gap-6 xl:gap-12 bg-off-white">
-	<h1 class="sr-only">Acclaim</h1>
+	<h1 class="sr-only">Press and Reviews</h1>
 	<section>
 		<figure class="w-full lg:w-3/4 2xl:w-2/3 mx-auto flex flex-col gap-2">
 			<img
