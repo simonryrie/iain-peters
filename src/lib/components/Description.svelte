4cb74666cda6dcc49fbd28ef4ce1400ffebd11fe
Simon Ryrie <simon@thisissoon.com>
2026-02-01 19:10:08 +0200

    feat(Description): Add sticker to cover

--- src/lib/components/Description.svelte
+++ src/lib/components/Description.svelte
@@ -6,7 +6,7 @@
 <div class="flex flex-col gap-6">
 	<DescriptionSection
 		{description}
-		src="/cover.png"
+		src="/cover-w-sticker.png"
 		alt="The front cover of The Corridor by Iain Peters"
 	/>
 </div>
